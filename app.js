fde20be8532c8a94e2b3e08f8f40ef7184c74c41
Akhmad Wildan <wildanakhmad3584@gmail.com>
2021-06-22 15:24:54 +0700

    baru berhasil menambah fitur cek username dan pasword

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts');
+const { body, validationResult } = require('express-validator');
+const {findUserName, findUserPassword} = require('./utility/login')
 const port = 3000
 
 // Gunakan ejs
@@ -8,7 +10,7 @@ app.set('view engine', 'ejs');
 
 // Built-in Midleware
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: false}));
 
 // third party midlleware
 app.use(expressLayouts);
@@ -40,6 +42,39 @@ app.get('/log_in', (req, res)=>{
         layout: 'layouts/main-layouts'
     })
 });
+
+// Proses Log in
+app.post('/', [
+    body('nama').custom((value,)=>{
+        const findName = findUserName(value);
+        if(!findName){
+            throw new Error('USername confirmation does not match name');
+        }
+        return true;
+    }),
+    body('password').custom((value)=>{
+        const findPassword = findUserPassword(value); 
+        if(!findPassword){
+            throw new Error('Password confirmation does not match Password');
+        }
+        return true;
+    })
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            // return res.status(400).json({ errors: errors.array() });
+            res.render('login',{
+                layout: 'layouts/main-layouts',
+                title: 'Halaman login',
+                css: 'css/login.css',
+                errors: errors.array()
+            });
+        } else {
+            // res.send('ok');
+            res.redirect('/');
+        }
+    });
 
 
 
--- utility/login.js
+++ utility/login.js
@@ -1,3 +1,4 @@
+const { json } = require('body-parser');
 const fs = require('fs');
 
 // membuat directory
@@ -12,3 +13,24 @@ const path = 'data/dataUser.json';
 if(!fs.existsSync(path)){
     fs.writeFileSync(path, '[]', 'utf-8');
 }
+
+// membaca data user
+const loadUser = () => {
+    const file = fs.readFileSync('data/dataUser.json', 'utf-8');
+    const users = JSON.parse(file);
+    return users;
+}
+
+const findUserName = (nama) => {
+    const users = loadUser();
+    const user = users.find((user) => user.nama === nama);
+    return user;
+}
+
+const findUserPassword = (password) => {
+    const users = loadUser();
+    const user = users.find((user) => user.password === password);
+    return user;
+}
+
+module.exports = { findUserName, findUserPassword };
